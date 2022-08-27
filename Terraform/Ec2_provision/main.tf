bf92b84da70132c8b0d77df5442541f1459f1619
techiehorizon <techiehorizon27@gmail.com>
2022-08-27 13:00:10 +0530

    Added Terraform

---
+++ Terraform/Ec2_provision/main.tf
@@ -0,0 +1,82 @@
+variable "awsprops" {
+    type = map
+    default = {
+    region = "us-east-1"
+    vpc = "vpc-0628db6f24b7e3120"
+    ami = "ami-05fa00d4c63e32376"
+    itype = "t2.micro"
+    subnet = "subnet-0c588dd9db06e8adf"
+    publicip = true
+    keyname = "techiehorzion"
+    secgroupname = "IAC-Sec-Group"
+  }
+}
+
+provider "aws" {
+  region = lookup(var.awsprops, "region")
+}
+
+resource "aws_security_group" "project-iac-sg" {
+  name = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_instance" "project-iac" {
+  ami = lookup(var.awsprops, "ami")
+  instance_type = lookup(var.awsprops, "itype")
+  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name = lookup(var.awsprops, "keyname")
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size = 8
+    volume_type = "gp2"
+  }
+  tags = {
+    Name ="Dev-01"
+    Environment = "DEV"
+    OS = "Amazon Linux"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
+}
+
+
+output "ec2instance" {
+  value = aws_instance.project-iac.public_ip
+}
